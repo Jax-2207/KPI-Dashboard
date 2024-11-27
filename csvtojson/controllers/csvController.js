0fa15926c01e158dd5fd4e942b2f7f9a65b2f534
Jay <jaywani22@gmail.com>
2024-11-27 19:00:11 +0530

    first commit

---
+++ csvtojson/controllers/csvController.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+const csv = require("fast-csv");
+const User = require("../Models/User")
+
+exports.create = async (req,res) => {
+    console.log(req.file):
+    try{
+
+    }catch(catch) {
+        res.status(400).json(error);
+    }
+};
